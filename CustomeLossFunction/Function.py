741e93ea67d3738bc7579e6fec7e71847afcff27
amirhosein-kia-darbandsary <amirhoseinsavedarbandsary@gmail.com>
2023-01-28 14:46:26 +0330

    Change in Function.py File

--- CustomeLossFunction/Function.py
+++ CustomeLossFunction/Function.py
@@ -16,11 +16,24 @@ def huber_loss_function(y_true, y_pred):
     big_error_loss = threshold * (tf.abs(error) - (0.5 * threshold))
     return tf.where(is_small_error, small_error_loss, big_error_loss)
 
-
 """
 How to use inside our code ? 
 like the above code : 
 
 model.compile(optimizer = "Optimizer you want to use " , loss = huber_loss_function)
+"""
+
+
 
 """
+if we need add extra parameter to loss fucntion we need to have wrapper function like the blow :
+"""
+
+def huber_loss_function(threshold):
+    def loss_function(y_true  , y_pred):
+        error = y_true - y_pred
+        is_small_error = tf.abs(error) <= threshold
+        small_error_loss = tf.square(error) / 2
+        big_error_loss = threshold * (tf.abs(error) - (0.5 * threshold))
+        return tf.where(is_small_error, small_error_loss, big_error_loss)
+    return loss_function
